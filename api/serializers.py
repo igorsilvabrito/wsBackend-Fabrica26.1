66e1c3ddaa7e97a1b175841e1f39bccf6613632a
Igor silva <igor.silvabrito@outlook.com>
2026-03-21 19:01:03 -0300

    feat adicionar MoedaFavoritaSerializer HistoricoCotacaoSerializer AlertaSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,32 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class MoedaFavoritaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MoedaFavorita
+        fields = ['id', 'par', 'apelido', 'criado_em']
+        read_only_fields = ['criado_em']
+
+    def create(self, validated_data):
+        usuario = self.context['request'].user
+        return MoedaFavorita.objects.create(usuario=usuario, **validated_data)
+
+
+class HistoricoCotacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HistoricoCotacao
+        fields = ['id', 'par', 'valor_compra', 'valor_venda', 'variacao', 'consultado_em']
+        read_only_fields = fields
+
+
+class AlertaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alerta
+        fields = ['id', 'par', 'tipo', 'valor_referencia', 'ativo', 'disparado', 'criado_em', 'disparado_em']
+        read_only_fields = ['disparado', 'criado_em', 'disparado_em']
+
+    def create(self, validated_data):
+        usuario = self.context['request'].user
+        return Alerta.objects.create(usuario=usuario, **validated_data)
